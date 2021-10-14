6e79b99ef6f635a161f7adea3bc7b765501a3cec
Bill <abcde5676151199@gmail.com>
2021-10-14 14:21:10 +0800

    viewBinding

--- app/src/main/java/com/example/ultimate_password/MainActivity.kt
+++ app/src/main/java/com/example/ultimate_password/MainActivity.kt
@@ -10,8 +10,14 @@ import com.example.ultimate_password.databinding.ActivityMainBinding
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        val secretNumber = binding.edSecretNumber.text.toString().toInt()
     }
+
 }
