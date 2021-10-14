ce2e5b59b5e85526f3a62cf10f13eae6d5eda322
Bill <abcde5676151199@gmail.com>
2021-10-14 14:14:40 +0800

    bulid MainActivity.kt & activity_main layout

---
+++ app/src/main/java/com/example/ultimate_password/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.ultimate_password
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.example.ultimate_password.databinding.ActivityMainBinding
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
