2db9a9dc60b72a26bc53078f99f1ff2f11cdd425
Bill <abcde5676151199@gmail.com>
2021-10-15 10:49:09 +0800

    fit guess.xml

--- app/src/main/java/com/example/ultimate_password/MainActivity.kt
+++ app/src/main/java/com/example/ultimate_password/MainActivity.kt
@@ -2,22 +2,15 @@ package com.example.ultimate_password
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.view.View
-import android.widget.Toast
-import androidx.appcompat.app.AlertDialog
 import com.example.ultimate_password.databinding.ActivityMainBinding
-import java.util.*
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+    lateinit var binding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val binding = ActivityMainBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
-        val secretNumber = binding.edSecretNumber.text.toString().toInt()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+''
     }
-
 }
